a656aea2c38e99a0d45fd6f6d3c7f151b97be4c8
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-04-11 23:40:47 +0200

    mediatek: tcpc_rt1711h: drop unused variables
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- drivers/misc/mediatek/typec/tcpc/tcpc_rt1711h.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_rt1711h.c
@@ -158,14 +158,20 @@ static int rt1711_read_device(void *client, u32 reg, int len, void *dst)
 {
 	struct i2c_client *i2c = client;
 	int ret = 0, count = 5;
+#if ENABLE_RT1711_DBG
 	u64 t1 = 0, t2 = 0;
+#endif
 
 	while (1) {
+#if ENABLE_RT1711_DBG
 		t1 = local_clock();
+#endif
 		ret = i2c_smbus_read_i2c_block_data(i2c, reg, len, dst);
+#if ENABLE_RT1711_DBG
 		t2 = local_clock();
 		RT1711_INFO("%s del = %lluus, reg = 0x%02X, len = %d\n",
 			    __func__, (t2 - t1) / NSEC_PER_USEC, reg, len);
+#endif
 		if (ret < 0 && count > 1)
 			count--;
 		else
@@ -179,14 +185,20 @@ static int rt1711_write_device(void *client, u32 reg, int len, const void *src)
 {
 	struct i2c_client *i2c = client;
 	int ret = 0, count = 5;
+#if ENABLE_RT1711_DBG
 	u64 t1 = 0, t2 = 0;
+#endif
 
 	while (1) {
+#if ENABLE_RT1711_DBG
 		t1 = local_clock();
+#endif
 		ret = i2c_smbus_write_i2c_block_data(i2c, reg, len, src);
+#if ENABLE_RT1711_DBG
 		t2 = local_clock();
 		RT1711_INFO("%s del = %lluus, reg = %02X, len = %d\n",
 			    __func__, (t2 - t1) / NSEC_PER_USEC, reg, len);
+#endif
 		if (ret < 0 && count > 1)
 			count--;
 		else
@@ -726,13 +738,13 @@ static inline int rt1711_fault_status_vconn_ov(struct tcpc_device *tcpc)
 
 int rt1711_fault_status_clear(struct tcpc_device *tcpc, uint8_t status)
 {
-	int ret;
+	int ret = 0;
 
 	if (status & TCPC_V10_REG_FAULT_STATUS_VCONN_OV)
 		ret = rt1711_fault_status_vconn_ov(tcpc);
 
 	rt1711_i2c_write8(tcpc, TCPC_V10_REG_FAULT_STATUS, status);
-	return 0;
+	return ret;
 }
 
 int rt1711_get_alert_mask(struct tcpc_device *tcpc, uint32_t *mask)
