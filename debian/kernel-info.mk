cfe063bc6b745865e2f20f790d2c1c76e1ad3148
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-09-07 15:24:34 +0200

    [packaging] kernel-info: use FLASH_INFO_ details from android13 vendor
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -170,12 +170,12 @@ FLASH_USE_TELNET = 0
 # Device manufacturer. This must match the `ro.product.vendor.manufacturer`
 # Android property. If you don't want to specify this, leave it undefined,
 # FLASH_INFO_CPU will be checked instead.
-FLASH_INFO_MANUFACTURER = Volla
+FLASH_INFO_MANUFACTURER = volla
 
 # Device model. This must match the `ro.product.vendor.model`
 # Android property. If you don't want to specify this, leave it undefined,
 # FLASH_INFO_CPU will be checked instead.
-FLASH_INFO_MODEL = Mimir
+FLASH_INFO_MODEL = Volla Tablet
 
 # Device CPU. This will be grepped against /proc/cpuinfo to check if
 # we're running on the specific device. Note this is a last-resort
